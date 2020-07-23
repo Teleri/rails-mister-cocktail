e7be695432933e23944c46020015c91ee73211af
Michael Knott <knottweb@gmail.com>
2020-07-23 21:45:37 +0100

    Nested routes for doses in cocktails, a little front-end added

--- app/controllers/doses_controller.rb
+++ app/controllers/doses_controller.rb
@@ -1,49 +1,29 @@
 class DosesController < ApplicationController
-  before_action :find_dose, only: [:show, :edit, :update, :destroy]
-
-  def index
-    @doses = Dose.all
-  end
-
-  def show
-    # code
-  end
-
   def new
+    @cocktail = Cocktail.find(params[:cocktail_id])
     @dose = Dose.new
   end
 
   def create
+    @cocktail = Cocktail.find(params[:cocktail_id])
     @dose = Dose.new(dose_params)
-    @dose.save!
+    @dose.cocktail = @cocktail
     if @dose.save
-      redirect_to dose_path(@dose)
+      redirect_to cocktail_path(@cocktail)
     else
       render :new
     end
   end
 
-  def edit
-    # code
-  end
-
-  def update
-    @dose.update(dose_params)
-    redirect_to dose_path(@dose)
-  end
-
   def destroy
-    @dose.destroy
-    redirect_to root_path
+    @cocktail = Cocktail.find(params[:cocktail_id])
+    @dose.destroy(@cocktail)
+    redirect_to cocktail_path(@cocktail)
   end
 
   private
 
-  def find_dose
-    @dose = Dose.find(params[:id])
-  end
-
   def dose_params
-    params.require(:dose).permit(:description, :cocktail, :ingredients)
+    params.require(:dose).permit(:description, :ingredient_id)
   end
 end
