8732212b604c301cb911bb8ee3adb4fa7ed332ab
Michael Knott <knottweb@gmail.com>
2020-07-25 14:55:38 +0100

    javascript to toggle showing and hiding the new dose form

--- app/javascript/toggle_new_dose_form.js
+++ app/javascript/toggle_new_dose_form.js
@@ -1,7 +1,16 @@
 function showNewDoseForm() {
-  const newDoseFormDiv = document.querySelector("#new-dose-form")
+  const newDoseFormDiv = document.querySelector("#new-dose-form");
+  const newDoseBtn = document.querySelector("#show-new-dose-form");
   newDoseFormDiv.toggleAttribute('hidden');
+  newDoseBtn.toggleAttribute('hidden');
 };
 
-const newDoseFormBtn = document.querySelector("#show-new-dose-form");
-newDoseFormBtn.addEventListener('toggle', showNewDoseForm);
+export function addToggleButton() {
+  const newDoseFormBtn = document.querySelector("#show-new-dose-form");
+  newDoseFormBtn.addEventListener('click', showNewDoseForm);
+};
+
+export function cancelButton() {
+  const newDoseBtn = document.querySelector("#cancel-button");
+  cancelButton.addEventListener('click', showNewDoseForm);
+};
